221da97f004edc7f44e2b4c4ea026cbb307929a0
srmichae <sjuseme22@gmail.com>
2026-09-18 07:34:37 -0500

    Ganador

--- Sistema_Votacion.py
+++ Sistema_Votacion.py
@@ -2,10 +2,25 @@ votos = {}
 
 def reiniciar_votacion():
     with open("historial.txt", "a") as archivo:
-        archivo.write(str(votos) + "\n")
+        for persona, candidato in votos.items():
+            archivo.write(candidato + "\n")
+
     votos.clear()
     print("Votación reiniciada.")
 
+def ganador():
+    conteo = {}
 
+    with open("historial.txt", "r") as archivo:
+        for candidato in archivo:
+            candidato = candidato.strip()
+            conteo[candidato] = conteo.get(candidato, 0) + 1
+
+    ganador = max(conteo, key=conteo.get)
+    print("Ganador:", ganador)
+
+votos["Michael"] = "A"
+votos["Juan"] = "B"
 
 reiniciar_votacion()
+ganador()
